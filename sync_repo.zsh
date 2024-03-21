764b0a2bae6c97e41c3f33a4210bdda15d644d68
robmllze <86869208+robmllze@users.noreply.github.com>
2024-03-21 16:07:03 +1100

    Update

---
+++ sync_repo.zsh
@@ -0,0 +1,21 @@
+##.title
+## ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+##
+## 🇽🇾🇿 & Dev
+##
+## Copyright Ⓒ Robert Mollentze, xyzand.dev
+## 
+## Licensing details can be found in the LICENSE file in the root directory.
+## 
+## ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+##.title~
+
+# Sync this folder with https://github.com/robmllze/___generators.
+cd ___generators
+git init
+git add .
+git commit -m "upstream"
+git remote remove upstream
+git remote add upstream https://github.com/robmllze/___generators.git
+git fetch upstream
+git merge upstream/main --allow-unrelated-histories --no-edit

7b7f8c629cacf6c16ab6d905837aed442e21a188
robmllze <86869208+robmllze@users.noreply.github.com>
2024-04-10 21:50:16 +1000

    Update

--- src/generate_screen.dart
+++ src/generate_screen.dart
@@ -17,10 +17,10 @@ import 'package:xyz_utils/xyz_utils.dart';
 const TARGET = 'apps/public_app';
 
 // To-Do: Give the screen class a name.
-const CLASS_NAME = 'NewProjectScreen';
+const CLASS_NAME = 'NewJobScreen';
 
 // To-Do: Provide a title for the Screen.
-const DEFAULT_TITLE = 'New Project';
+const DEFAULT_TITLE = 'New Job';
 
 // To-Do: Specify the Screen's access settings:
 const bool IS_ONLY_ACCESSIBLE_IF_LOGGED_IN_AND_VERIFIED = false;

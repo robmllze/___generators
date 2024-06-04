a695ca4f46b2c14e933a96fbc9f7afa2f8e8c368
robmllze <86869208+robmllze@users.noreply.github.com>
2024-06-04 13:19:39 +1000

    Bugfix

--- src/generate_makeups.dart
+++ src/generate_makeups.dart
@@ -1,82 +1,82 @@
-//.title
-// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//
-// 🇽🇾🇿 & Dev
-//
-// Licencing details are in the LICENSE file in the root directory.
-//
-// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//.title~
+// //.title
+// // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+// //
+// // 🇽🇾🇿 & Dev
+// //
+// // Licencing details are in the LICENSE file in the root directory.
+// //
+// // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+// //.title~
 
-import 'package:xyz_gen/xyz_gen.dart';
-import 'package:xyz_utils/xyz_utils.dart';
+// import 'package:xyz_gen/xyz_gen.dart';
+// import 'package:xyz_utils/xyz_utils.dart';
 
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-// To-Do: List the folders their subfolders that may hold your widget classes
-// that you annotated with @GenerateMakeup, in order to generate their
-// respective makeup classes.
-const folders = <String>{
-  '',
-  '_view',
-  'apps/admin_app',
-  'apps/operations_app',
-  'apps/public_app',
-};
+// // To-Do: List the folders their subfolders that may hold your widget classes
+// // that you annotated with @GenerateMakeup, in order to generate their
+// // respective makeup classes.
+// const folders = <String>{
+//   'example',
+//   '_view',
+//   'apps/admin_app',
+//   'apps/operations_app',
+//   'apps/public_app',
+// };
 
-const subfolders = <String>{
-  'lib/src/widgets',
-  'lib/src/components',
-  'lib/src/makeups',
-  'lib/widgets',
-  'lib/components',
-  'lib/makeups',
-};
+// const subfolders = <String>{
+//   'lib/src/widgets',
+//   'lib/src/components',
+//   'lib/src/makeups',
+//   'lib/widgets',
+//   'lib/components',
+//   'lib/makeups',
+// };
 
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-//
-// DO NOT MODIFY BELOW
-//
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// //
+// // DO NOT MODIFY BELOW
+// //
+// // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-void main() async {
-  await _generateModels();
-  await _generateExports();
-}
+// void main() async {
+//   await _generateModels();
+//   await _generateExports();
+// }
 
-// -----------------------------------------------------------------------------
+// // -----------------------------------------------------------------------------
 
-Future<void> _generateModels() async {
-  DebugLog.debugOnly = false;
-  for (final folder in folders) {
-    await generateMakeupsApp([
-      '--builder-template',
-      '$currentScriptDir/templates/generate_makeups/your_makeup_builder_template.dart.md',
-      '--class-template',
-      '$currentScriptDir/templates/generate_makeups/your_makeup_class_template.dart.md',
-      '--theme-template',
-      '$currentScriptDir/templates/generate_makeups/your_generated_theme_template.dart.md',
-      '--generate-template',
-      '$currentScriptDir/templates/generate_makeups/your_makeup_generate_template.dart.md',
-      '-r',
-      '$currentScriptDir/../../$folder',
-      '-s',
-      subfolders.join('&'),
-      '--output',
-      '$currentScriptDir/../../$folder/makeups',
-    ]);
-  }
-}
+// Future<void> _generateModels() async {
+//   DebugLog.debugOnly = false;
+//   for (final folder in folders) {
+//     await generateMakeupsApp([
+//       '--builder-template',
+//       '$currentScriptDir/templates/generate_makeups/your_makeup_builder_template.dart.md',
+//       '--class-template',
+//       '$currentScriptDir/templates/generate_makeups/your_makeup_class_template.dart.md',
+//       '--theme-template',
+//       '$currentScriptDir/templates/generate_makeups/your_generated_theme_template.dart.md',
+//       '--generate-template',
+//       '$currentScriptDir/templates/generate_makeups/your_makeup_generate_template.dart.md',
+//       '-r',
+//       '$currentScriptDir/../../$folder',
+//       '-s',
+//       subfolders.join('&'),
+//       '--output',
+//       '$currentScriptDir/../../$folder/makeups',
+//     ]);
+//   }
+// }
 
-// -----------------------------------------------------------------------------
+// // -----------------------------------------------------------------------------
 
-Future<void> _generateExports() async {
-  await generateExportsApp([
-    '-t',
-    '$currentScriptDir/templates/generate_exports/your_exports_template.dart.md',
-    '-r',
-    folders.map((e) => '$currentScriptDir/../../${e.isNotEmpty ? '$e/' : ''}').join('&'),
-    '-s',
-    subfolders.join('&'),
-  ]);
-}
+// Future<void> _generateExports() async {
+//   await generateExportsApp([
+//     '-t',
+//     '$currentScriptDir/templates/generate_exports/your_exports_template.dart.md',
+//     '-r',
+//     folders.map((e) => '$currentScriptDir/../../${e.isNotEmpty ? '$e/' : ''}').join('&'),
+//     '-s',
+//     subfolders.join('&'),
+//   ]);
+// }
